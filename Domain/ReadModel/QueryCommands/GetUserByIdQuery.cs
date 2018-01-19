88e06ba285d7581cd56e6d5c69fe5512f997bcff
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-01-19 07:49:44 -0500

    Added GetUserByIdQuery and associated handler

---
+++ Domain/ReadModel/QueryCommands/GetUserByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UrbanSpork.Domain.SLCQRS.ReadModel;
+
+namespace UrbanSpork.Domain.ReadModel.QueryCommands
+{
+    public class GetUserByIdQuery : IQuery<Task<List<JObject>>>
+    {
+        internal int Id;
+
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
