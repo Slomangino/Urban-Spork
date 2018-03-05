0714e169004935c945d739ba0ec79bb1b1473ccb
T Hall <jamesthall90@gmail.com>
2018-03-04 21:17:29 -0500

    - Created IFilterCriteria interface to be implemented by any FilterCriteria created - Completed Filtering functionality in GetUserCollectionQueryHandler, aside from sorting by Date and Permissions, and filtering permissions

---
+++ CQRSLite/Filters/IFilterCriteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanSpork.CQRS.Filters
+{
+    public interface IFilterCriteria
+    {
+        string SearchTerms { get; set; }
+
+        string SortDirection { get; set; }
+
+        string SortField { get; set; }
+
+        bool EnablePaging { get; set; }
+    }
+}
--- UrbanSpork.ReadModel/QueryHandlers/GetUserCollectionQueryHandler.cs
+++ UrbanSpork.ReadModel/QueryHandlers/GetUserCollectionQueryHandler.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Extensions.Internal;
 using UrbanSpork.Common.DataTransferObjects;
+using UrbanSpork.Common.FilterCriteria;
 using UrbanSpork.CQRS.Queries.QueryHandler;
 using UrbanSpork.DataAccess.DataAccess;
 using UrbanSpork.DataAccess.Projections;
@@ -17,6 +18,7 @@ namespace UrbanSpork.ReadModel.QueryHandlers
     public class GetUserCollectionQueryHandler : IQueryHandler<GetUserCollectionQuery, List<UserDetailProjection>>
     {
         private readonly UrbanDbContext _context;
+        private string _searchTerm = "";
 
         public GetUserCollectionQueryHandler(UrbanDbContext context)
         {
@@ -25,127 +27,151 @@ namespace UrbanSpork.ReadModel.QueryHandlers
 
         public async Task<List<UserDetailProjection>> Handle(GetUserCollectionQuery query)
         {
-            IQueryable<UserDetailProjection> queryable = _context.UserDetailProjection;
-
-            if (!query.FilterCriteria.IncludeInactive)
-            {
-                queryable = queryable.Where(a => a.IsActive);
-            }
-
-            if (!query.FilterCriteria.IncludeIsAdmin)
-            {
-                queryable = queryable.Where(a => a.IsAdmin);
-            }
-
-
-            //if (!query.FilterCriteria.SearchTerms.Equals(""))
-            //{
-            //    queryable = queryable.Where(a => a.Department.Contains(query.FilterCriteria.SearchTerms));
-            //    queryable = queryable.Where(a => a.FirstName.Contains(query.FilterCriteria.SearchTerms));
-            //    queryable = queryable.Where(a => a.LastName.Contains(query.FilterCriteria.SearchTerms));
-            //    queryable = queryable.Where(a => a.Position.Contains(query.FilterCriteria.SearchTerms));
-            //    queryable = queryable.Where(a => a.Department.Contains(query.FilterCriteria.SearchTerms));
-            //}
-
-
-
-            if (query.FilterCriteria.SortDirection.Equals("ASC"))
-            {
-                switch (query.FilterCriteria.SortField)
-                {
-                    case "Email":
-                        queryable = queryable.OrderBy(a => a.Email)
-                            .ThenBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                    case "LastName":
-                        queryable = queryable.OrderBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                    case "FirstName":
-                        queryable = queryable.OrderBy(a => a.FirstName)
-                            .ThenBy(a => a.LastName);
-                        break;
-                    case "IsAdmin":
-                        queryable = queryable.OrderBy(a => a.IsAdmin)
-                            .ThenBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                    case "DateCreated":
-                        queryable = queryable.OrderBy(a => a.DateCreated)
-                            .ThenBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                    case "Position":
-                        queryable = queryable.OrderBy(a => a.Position)
-                            .ThenBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                    case "Department":
-                        queryable = queryable.OrderBy(a => a.Department)
-                            .ThenBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                            
-                        break;
-                    default:
-                        queryable = queryable.OrderBy(a => a.LastName)
-                            .ThenBy(a => a.FirstName);
-                        break;
-                }
-            }
-            else
-            {
-                switch (query.FilterCriteria.SortField)
-                {
-                    case "Email":
-                        queryable = queryable.OrderByDescending(a => a.Email)
-                            .ThenByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    case "LastName":
-                        queryable = queryable.OrderByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    case "FirstName":
-                        queryable = queryable.OrderByDescending(a => a.FirstName)
-                            .ThenByDescending(a => a.LastName);
-                        break;
-                    case "IsAdmin":
-                        queryable = queryable.OrderByDescending(a => a.IsAdmin)
-                            .ThenByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    case "DateCreated":
-                        queryable = queryable.OrderByDescending(a => a.DateCreated)
-                            .ThenByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    case "Position":
-                        queryable = queryable.OrderByDescending(a => a.Position)
-                            .ThenByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    case "Department":
-                        queryable = queryable.OrderByDescending(a => a.Department)
-                            .ThenByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                    default:
-                        queryable = queryable.OrderByDescending(a => a.LastName)
-                            .ThenByDescending(a => a.FirstName);
-                        break;
-                }
-            }
-
-            var result = await queryable.ToListAsync();
-
+            
+            var result = await Filter(query.FilterCriteria).ToListAsync();
             
             //var map = Mapper.Map<List<UserDetailProjection>, List<UserDTO>>(result);
             //return map;
 
             return result;
+        }
 
-            //throw new NotImplementedException();
+        private IQueryable<UserDetailProjection> Filter(UserFilterCriteria filter)
+        {
+            IQueryable<UserDetailProjection> queryable = _context.UserDetailProjection;
+
+            _searchTerm = filter.SearchTerms.ToLower();
+
+            if (!filter.IncludeInactive)
+            {
+                queryable = queryable.Where(a => a.IsActive);
+            }
+
+            if (!filter.IncludeIsAdmin)
+            {
+                queryable = queryable.Where(a => !a.IsAdmin);
+            }
+
+            if (!String.IsNullOrWhiteSpace(_searchTerm))
+            {
+                queryable = queryable.Where(
+                    a => a.Department.ToLower().Contains(_searchTerm) ||
+                    a.FirstName.ToLower().Contains(_searchTerm) ||
+                    a.LastName.ToLower().Contains(_searchTerm) ||
+                    a.Department.ToLower().Contains(_searchTerm) ||
+                    a.Position.ToLower().Contains(_searchTerm) ||
+                    a.Department.ToLower().Contains(_searchTerm)
+                );
+            }
+
+            if (filter.SortDirection.Equals("ASC"))
+            {
+                switch (filter.SortField)
+                {
+                    case "Email":
+                        queryable = queryable.OrderBy(a => a.Email);
+                        //.ThenBy(a => a.LastName)
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    case "LastName":
+                        queryable = queryable.OrderBy(a => a.LastName);
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    case "FirstName":
+                        queryable = queryable.OrderBy(a => a.FirstName);
+                        //.ThenBy(a => a.LastName);
+                        break;
+                    case "IsAdmin":
+                        queryable = queryable.OrderBy(a => a.IsAdmin);
+                        //.ThenBy(a => a.LastName)
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    case "DateCreated":
+                        queryable = queryable.OrderBy(a => a.DateCreated);
+                        //.ThenBy(a => a.LastName)
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    case "Position":
+                        queryable = queryable.OrderBy(a => a.Position);
+                        //.ThenBy(a => a.LastName)
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    case "Department":
+                        queryable = queryable.OrderBy(a => a.Department);
+                        //.ThenBy(a => a.LastName)
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                    default:
+                        queryable = queryable.OrderBy(a => a.LastName);
+                        //.ThenBy(a => a.FirstName);
+                        break;
+                }
+            }
+            else
+            {
+                switch (filter.SortField)
+                {
+                    case "Email":
+                        queryable = queryable.OrderByDescending(a => a.Email);
+                        //.ThenByDescending(a => a.LastName)
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    case "LastName":
+                        queryable = queryable.OrderByDescending(a => a.LastName);
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    case "FirstName":
+                        queryable = queryable.OrderByDescending(a => a.FirstName);
+                        //.ThenByDescending(a => a.LastName);
+                        break;
+                    case "IsAdmin":
+                        queryable = queryable.OrderByDescending(a => a.IsAdmin);
+                        //.ThenByDescending(a => a.LastName)
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    case "DateCreated":
+                        queryable = queryable.OrderByDescending(a => a.DateCreated);
+                        //.ThenByDescending(a => a.LastName)
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    case "Position":
+                        queryable = queryable.OrderByDescending(a => a.Position);
+                        //.ThenByDescending(a => a.LastName)
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    case "Department":
+                        queryable = queryable.OrderByDescending(a => a.Department);
+                        //.ThenByDescending(a => a.LastName)
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                    default:
+                        queryable = queryable.OrderByDescending(a => a.LastName);
+                        //.ThenByDescending(a => a.FirstName);
+                        break;
+                }
+            }
+
+            //Filter For Min/Max Date Range
+            if (filter.MinDate != null && filter.MaxDate != null)
+            {
+                queryable = queryable.Where(a =>
+                    a.DateCreated >= filter.MinDate &&
+                    a.DateCreated <= filter.MaxDate);
+            }
+
+            //Filter For MinDate Only
+            else if (filter.MinDate != null && filter.MaxDate == null)
+            {
+                queryable = queryable.Where(a => a.DateCreated >= filter.MinDate);
+            }
+
+            //Filter For MaxDate Only
+            else if (filter.MinDate == null && filter.MaxDate != null)
+            {
+                queryable = queryable.Where(a => a.DateCreated <= filter.MaxDate);
+            }
+
+            return queryable;
         }
     }
 }
