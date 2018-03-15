b920f46d50d20b9dfd56f19b91293ff078f56d6e
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-03-14 21:48:59 -0400

    Completed PermissionAggregate Unit tests

--- api/UrbanSpork.Tests/Permission/PermissionAggregateTests.cs
+++ api/UrbanSpork.Tests/Permission/PermissionAggregateTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UrbanSpork.Common.DataTransferObjects.Permission;
+using UrbanSpork.Common.DataTransferObjects.User;
 using Xunit;
 using UrbanSpork.DataAccess;
 using UrbanSpork.DataAccess.Events;
@@ -35,14 +36,39 @@ namespace UrbanSpork.Tests.Permission
                 var @event = (PermissionCreatedEvent) e;
                 Assert.Equal("test", @event.Name);
                 Assert.Equal("test", @event.Description);
-                Assert.True(@event.IsActive);
             });
 
             Assert.NotEqual(Guid.Empty, agg.Id);
             Assert.Equal("test", agg.Name);
             Assert.Equal("test", agg.Description);
-            Assert.True(agg.IsActive);
 
+        }
+
+        [Fact]
+        public void given_new_permission_should_be_enabled_after_permission_created_event()
+        {
+            //assemble
+            CreateNewPermissionDTO input = new CreateNewPermissionDTO
+            {
+                Name = "test",
+                Description = "test",
+                IsActive = true
+            };
+
+            //apply
+            var agg = PermissionAggregate.CreateNewPermission(input);
+
+            //assert
+            var changes = agg.GetUncommittedChanges();
+            Assert.Single(changes);
+            Assert.Collection(changes,
+                (e) =>
+                {
+                    Assert.IsType<PermissionCreatedEvent>(e);
+                    var @event = (PermissionCreatedEvent)e;
+                    Assert.True(@event.IsActive);
+                });
+            Assert.True(agg.IsActive);
         }
 
         [Fact]
@@ -62,7 +88,7 @@ namespace UrbanSpork.Tests.Permission
 
             //assert
             var changes = agg.GetUncommittedChanges();
-            Assert.Equal(2, changes.Count());
+            Assert.Equal(2, changes.Length);
             Assert.Collection(changes, 
                 (e) =>
                 {
@@ -75,6 +101,83 @@ namespace UrbanSpork.Tests.Permission
                     Assert.False(@event.IsActive);
                 });
             Assert.False(agg.IsActive);
+        }
+
+        [Fact]
+        public void given_disabled_permission_should_be_enabled_after_permission_enabled_event()
+        {
+            //assemble
+            CreateNewPermissionDTO input = new CreateNewPermissionDTO
+            {
+                Name = "test",
+                Description = "test",
+                IsActive = false
+            };
+            var agg = PermissionAggregate.CreateNewPermission(input);
+
+            //apply
+            agg.EnablePermission();
+
+            //Assert
+            var changes = agg.GetUncommittedChanges();
+            Assert.Equal(2, changes.Length);
+            Assert.Collection(changes,
+                (e) =>
+                {
+                    Assert.IsType<PermissionCreatedEvent>(e);
+                    var @event = (PermissionCreatedEvent) e;
+                    Assert.False(@event.IsActive);
+                },
+                (e) =>
+                {
+                    Assert.IsType<PermissionEnabledEvent>(e);
+                    var @event = (PermissionEnabledEvent) e;
+                    Assert.True(@event.IsActive);
+                });
+            Assert.True(agg.IsActive);
+        }
+
+        [Fact]
+        public void given_new_permission_should_have_correct_values_after_update_permission_event()
+        {
+            //assemble
+            CreateNewPermissionDTO input = new CreateNewPermissionDTO
+            {
+                Name = "test",
+                Description = "test",
+                IsActive = true
+            };
+            var agg = PermissionAggregate.CreateNewPermission(input);
+            var userAgg = UserAggregate.CreateNewUser(new CreateUserInputDTO());
+
+            var updateInfo = new UpdatePermissionInfoDTO
+            {
+                Id = agg.Id,
+                UpdatedById = userAgg.Id,
+                Name = "New Name",
+                Description = "New Description",
+            };
+
+            //apply
+            agg.UpdatePermissionInfo(updateInfo);
+
+            //assert
+            var changes = agg.GetUncommittedChanges();
+            Assert.Equal(2, changes.Length);
+            Assert.Collection(changes,
+                (e) =>
+                {
+                    Assert.IsType<PermissionCreatedEvent>(e);
+                },
+                (e) =>
+                {
+                    Assert.IsType<PermissionInfoUpdatedEvent>(e);
+                    var @event = (PermissionInfoUpdatedEvent)e;
+                    Assert.Equal(@event.Name, updateInfo.Name);
+                    Assert.Equal(@event.Description, updateInfo.Description);
+                });
+            Assert.Equal(agg.Name, updateInfo.Name);
+            Assert.Equal(agg.Description, updateInfo.Description);
         }
     }
 }
