35146b6f004d450c2ad84ac57902832df687f1a7
Tyler <thanse80@gmail.com>
2018-03-22 12:13:01 -0400

    added IEmail

---
+++ api/UrbanSpork.DataAccess/Emails/IEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanSpork.Common.DataTransferObjects;
+
+namespace UrbanSpork.DataAccess.Emails
+{
+    public interface IEmail
+    {
+        void SendUserCreatedMessage(UserDTO user);
+        void SendPermissionsRequestedMessage(UserAggregate forAgg, List<PermissionAggregate> permissions);
+        void SendRequestDeniedMessage(UserAggregate forAgg, List<PermissionAggregate> deniedPermissions);
+        void SendPermissionsGrantedMessage(UserAggregate forAgg, List<PermissionAggregate> grantedPermissions);
+        void SendPermissionsRevokedMessage(UserAggregate forAgg, List<PermissionAggregate> revokedPermissions);
+    }
+}
