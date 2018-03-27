5ec6aad8aeae0b61f1269e38eb85bb4125c7fd51
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-03-26 21:07:53 -0400

    soft Finished DashboardProjection, needs testing merging into template branch to test.

--- api/UrbanSpork.DataAccess/GenericEventPublisher.cs
+++ api/UrbanSpork.DataAccess/GenericEventPublisher.cs
@@ -15,6 +15,7 @@ namespace UrbanSpork.DataAccess
         private readonly UserManagementProjection _userManagementProjection;
         private readonly ApproverActivityProjection _approverActivityProjection;
         private readonly SystemActivityProjection _systemActivityProjection;
+        private readonly DashboardProjection _dashBoardProjection;
 
         public GenericEventPublisher(
             UserDetailProjection userDetailProjection, 
@@ -23,7 +24,8 @@ namespace UrbanSpork.DataAccess
             SystemDropdownProjection systemDropdownProjection, 
             UserManagementProjection userManagementProjection,
             SystemActivityProjection systemActivityProjection,
-            ApproverActivityProjection approverActivityProjection
+            ApproverActivityProjection approverActivityProjection,
+            DashboardProjection dashboardProjection
             )
         {
             _userDetailProjection = userDetailProjection;
@@ -33,6 +35,7 @@ namespace UrbanSpork.DataAccess
             _systemDropdownProjection = systemDropdownProjection;
             _approverActivityProjection = approverActivityProjection;
             _systemActivityProjection = systemActivityProjection;
+            _dashBoardProjection = dashboardProjection;
         }
 
         async Task IEventPublisher.Publish<T>(T @event, CancellationToken cancellationToken)
@@ -45,6 +48,7 @@ namespace UrbanSpork.DataAccess
             await _userManagementProjection.ListenForEvents(@event);
             await _approverActivityProjection.ListenForEvents(@event);
             await _systemActivityProjection.ListenForEvents(@event);
+            await _dashBoardProjection.ListenForEvents(@event);
         }
     }
 }
--- api/UrbanSpork.DataAccess/Projections/DashboardProjection.cs
+++ api/UrbanSpork.DataAccess/Projections/DashboardProjection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using UrbanSpork.CQRS.Events;
@@ -22,7 +23,6 @@ namespace UrbanSpork.DataAccess.Projections
         }
 
         [Key]
-        public Guid Id { get; set; }
         public Guid PermissionId { get; set; }
         public int ActiveUsers { get; set; }
         public int PendingRequests { get; set; }
@@ -35,30 +35,93 @@ namespace UrbanSpork.DataAccess.Projections
 
             switch (@event)
             {
-                case PermissionInfoUpdatedEvent pIUEvent:
-                    dBProjection = await _context.DashBoardProjection.SingleAsync(a => a.PermissionId == pIUEvent.Id);
+                case PermissionInfoUpdatedEvent pue:
+                    dBProjection = await _context.DashBoardProjection.SingleAsync(a => a.PermissionId == pue.Id);
                     _context.Attach(dBProjection);
 
-                    dBProjection.SystemName = pIUEvent.Name;
-                    dBProjection.LogoUrl = pIUEvent.Image;
+                    dBProjection.SystemName = pue.Name;
+                    dBProjection.LogoUrl = pue.Image;
+
                     _context.Entry(dBProjection).Property(a => a.SystemName).IsModified = true;
                     _context.Entry(dBProjection).Property(a => a.LogoUrl).IsModified = true;
+
                     _context.DashBoardProjection.Update(dBProjection);
                     break;
-                case PermissionCreatedEvent pCEvent:
-                    dBProjection.Id = new Guid();
-                    dBProjection.PermissionId = pCEvent.Id;
-                    dBProjection.SystemName = pCEvent.Name;
+                case PermissionCreatedEvent pce:
+                    dBProjection.PermissionId = pce.Id;
+                    dBProjection.SystemName = pce.Name;
                     dBProjection.ActiveUsers = 0;
                     dBProjection.PendingRequests = 0;
-                    dBProjection.LogoUrl = pCEvent.Image;
+                    dBProjection.LogoUrl = pce.Image;
+                    _context.DashBoardProjection.Add(dBProjection);
                     break;
-                case UserPermissionsRequestedEvent uPermReqestedEvent:
-                    //List<DashboardProjection> rowsToBeUpdated = await _context.DashBoardProjection.SingleAsync(a => a.PermissionId == uPermReqestedEvent.Requests.);
-
-
-                    _context.Attach(dBProjection);
+                case UserPermissionsRequestedEvent pre:
+                    //increment pending requests
+                    var rowsToBeUpdated = await _context.DashBoardProjection.Where(a => pre.Requests.ContainsKey(a.PermissionId)).ToListAsync();
+                    foreach (var entry in rowsToBeUpdated)
+                    {
+                        entry.PendingRequests++;
+                        _context.Entry(entry).Property(a => a.PendingRequests).IsModified = true;
+                        _context.Update(entry);
+                    }
                     break;
+                case UserPermissionRequestDeniedEvent prd:
+                    //decrement pending requests
+                    var rows = await _context.DashBoardProjection.Where(a => prd.PermissionsToDeny.ContainsKey(a.PermissionId)).ToListAsync();
+                    foreach (var entry in rows)
+                    {
+                        if (entry.PendingRequests <= 1)
+                        {
+                            entry.PendingRequests = 0;
+                        }
+                        else
+                        {
+                            entry.PendingRequests-- ;
+                        }
+                        _context.Entry(entry).Property(a => a.PendingRequests).IsModified = true;
+                        _context.Update(entry);
+                    }
+                    break;
+                case UserPermissionGrantedEvent pge:
+                    //increment active users
+                    var rs = await _context.DashBoardProjection.Where(a => pge.PermissionsToGrant.ContainsKey(a.PermissionId)).ToListAsync();
+                    foreach (var entry in rs)
+                    {
+                        entry.ActiveUsers++;
+
+                        if (entry.PendingRequests <= 1)
+                        {
+                            entry.PendingRequests = 0;
+                        }
+                        else
+                        {
+                            entry.PendingRequests--;
+                        }
+
+                        _context.Entry(entry).Property(a => a.ActiveUsers).IsModified = true;
+                        _context.Entry(entry).Property(a => a.PendingRequests).IsModified = true;
+                        _context.Update(entry);
+                    }
+                    break;
+                case UserPermissionRevokedEvent upr:
+                    //decrement active users
+                    var r = await _context.DashBoardProjection.Where(a => upr.PermissionsToRevoke.ContainsKey(a.PermissionId)).ToListAsync();
+                    foreach (var entry in r)
+                    {
+                        if (entry.ActiveUsers <= 1)
+                        {
+                            entry.ActiveUsers = 0;
+                        }
+                        else
+                        {
+                            entry.ActiveUsers--;
+                        }
+
+                        _context.Entry(entry).Property(a => a.ActiveUsers).IsModified = true;
+                        _context.Update(entry);
+                    }
+                    break;
+
             }
             await _context.SaveChangesAsync();
         }
