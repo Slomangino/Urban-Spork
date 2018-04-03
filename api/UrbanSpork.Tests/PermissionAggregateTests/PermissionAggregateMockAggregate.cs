67fe8393f0f90772b5b68630f1a976154e3f2434
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-04-02 21:12:22 -0400

    Created MockAggregate for permission Aggregate, Fixed PermissionAggregateTests

---
+++ api/UrbanSpork.Tests/PermissionAggregateTests/PermissionAggregateMockAggregate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanSpork.Common.DataTransferObjects.User;
+using UrbanSpork.DataAccess;
+
+namespace UrbanSpork.Tests.PermissionAggregateTests
+{
+    public static class PermissionAggregateMockAggregate
+    {
+        public static UserAggregate SetupAdminUser()
+        {
+
+            var dto = new CreateUserInputDTO
+            {
+                FirstName = "Stephen",
+                LastName = "Lomangino",
+                Email = "iceman371@yahoo.com",
+                Position = "API Lead",
+                Department = "Development",
+                IsAdmin = true,
+                IsActive = true,
+
+                PermissionList = { }
+            };
+
+            var agg = UserAggregate.CreateNewUser(dto);
+            return agg;
+        }
+    }
+}
