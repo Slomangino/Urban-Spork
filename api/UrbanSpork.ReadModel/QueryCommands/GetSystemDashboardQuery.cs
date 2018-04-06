5936c87e4bfd1b191f767abf83793f15afd8a8be
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-04-06 14:29:02 -0400

    Implemented GetSystemDashboardQuery because it was forgotten when the DashboardProjection was originally implemented.

---
+++ api/UrbanSpork.ReadModel/QueryCommands/GetSystemDashboardQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanSpork.Common.FilterCriteria;
+using UrbanSpork.CQRS.Queries.Query;
+using UrbanSpork.CQRS.Queries.QueryHandler;
+using UrbanSpork.DataAccess.Projections;
+
+namespace UrbanSpork.ReadModel.QueryCommands
+{
+    public class GetSystemDashboardQuery : IQuery<List<DashboardProjection>>
+    {
+        private readonly DashboardFilterCriteria filter;
+
+        public GetSystemDashboardQuery(DashboardFilterCriteria filter)
+        {
+            this.filter = filter;
+        }
+    }
+}
---
+++ api/UrbanSpork.ReadModel/QueryHandlers/GetSystemDashboardQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UrbanSpork.CQRS.Queries.QueryHandler;
+using UrbanSpork.DataAccess.DataAccess;
+using UrbanSpork.DataAccess.Projections;
+using UrbanSpork.ReadModel.QueryCommands;
+
+namespace UrbanSpork.ReadModel.QueryHandlers
+{
+    public class GetSystemDashboardQueryHandler : IQueryHandler<GetSystemDashboardQuery, List<DashboardProjection>>
+    {
+        private readonly UrbanDbContext _context;
+
+        public GetSystemDashboardQueryHandler(UrbanDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<DashboardProjection>> Handle(GetSystemDashboardQuery query)
+        {
+            IQueryable<DashboardProjection> queryable = _context.DashBoardProjection;
+
+            //ordering by most pending requests and then by most active users
+            queryable = queryable.OrderByDescending(a => a.PendingRequests)
+                .ThenByDescending(a => a.ActiveUsers);
+
+            var list = await queryable.ToListAsync();
+
+            //return whole list if it is less than 15 entries
+            if (list.Count < 15)
+            {
+                return list;
+            }
+
+            //otherwise return the truncated list of top 15 results
+            return list.GetRange(0, 14);
+        }
+    }
+}
