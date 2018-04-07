79a9d41b2361bea6c08bde62821cd7e0067564c7
T Hall <jamesthall90@gmail.com>
2018-04-07 19:37:12 -0400

    - Added some UserControllerTests

---
+++ api/UrbanSpork.Tests/Controllers/User/UserControllerMockAggregate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using UrbanSpork.API.Controllers;
+using UrbanSpork.Common.DataTransferObjects.Permission;
+using UrbanSpork.Common.DataTransferObjects.User;
+using UrbanSpork.CQRS.Queries;
+using UrbanSpork.CQRS.Queries.Query;
+using UrbanSpork.CQRS.WriteModel;
+using UrbanSpork.ReadModel.QueryCommands;
+
+namespace UrbanSpork.Tests.Controllers.User
+{
+    class UserControllerMockAggregate
+    {
+        #region Mocks
+        private static readonly Mock<ICommandDispatcher> CommandDispatcherMock = new Mock<ICommandDispatcher>();
+
+        public static readonly Mock<IQueryProcessor> QueryProcessorMock = new Mock<IQueryProcessor>();
+
+        #endregion
+
+        #region Objects
+
+        public readonly ICommandDispatcher CommandDispatcher = CommandDispatcherMock.Object;
+
+        public readonly IQueryProcessor QueryProcessor = QueryProcessorMock.Object;
+
+        #endregion
+
+        #region Commands & Queries
+
+        public IQuery<UserDetailProjectionDTO> UserQuery;
+
+        public IQuery<List<UserDetailProjectionDTO>> UserCollectionQuery;
+
+        public IQuery<OffBoardUserDTO> OffboardQuery;
+
+
+        #endregion
+
+        #region Factories
+
+        public UserController CreateUserController()
+        {
+            return new UserController(QueryProcessor, CommandDispatcher);
+        }
+        
+        #endregion
+
+        #region Setup Methods
+
+        public void setup_processor_to_verify_getUserQueries_are_the_same(Guid id)
+        {
+            QueryProcessorMock.Setup(a => a.Process(It.IsAny<GetUserDetailByIdQuery>()))
+                .Callback<IQuery<UserDetailProjectionDTO>>((a) => { UserQuery = a; })
+                .ReturnsAsync(new UserDetailProjectionDTO
+                {
+                    UserId = id
+                });
+        }
+
+        public void setup_processor_to_verify_getUserCollectionQueries_are_the_same()
+        {
+            QueryProcessorMock.Setup(a => a.Process(It.IsAny<GetUserCollectionQuery>()))
+                .Callback<IQuery<List<UserDetailProjectionDTO>>>((a) => { UserCollectionQuery = a; })
+                .ReturnsAsync(new List<UserDetailProjectionDTO>());
+        }
+
+        public void setup_processor_to_verify_getUserOffboardingQueries_are_the_same()
+        {
+            QueryProcessorMock.Setup(a => a.Process(It.IsAny<GetOffboardUserPermissionsQuery>()))
+                .Callback<IQuery<OffBoardUserDTO>>((a) => { OffboardQuery = a; })
+                .ReturnsAsync(new OffBoardUserDTO());
+        }
+
+        #endregion
+    }
+}
---
+++ api/UrbanSpork.Tests/Controllers/User/UserControllerTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanSpork.Common;
+using UrbanSpork.Common.DataTransferObjects.Permission;
+using UrbanSpork.Common.DataTransferObjects.User;
+using UrbanSpork.Common.FilterCriteria;
+using UrbanSpork.DataAccess.DataAccess;
+using UrbanSpork.DataAccess.Events;
+using UrbanSpork.DataAccess.Events.Users;
+using UrbanSpork.DataAccess.Projections;
+using UrbanSpork.ReadModel.QueryCommands;
+using UrbanSpork.Tests.Controllers.Permission;
+using Xunit;
+
+namespace UrbanSpork.Tests.Controllers.User
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public async void given_getuserbyid_query_queryprocessor_should_get_same_query_created_in_controller()
+        {
+            //Assemble
+            var mockAgg = new UserControllerMockAggregate();
+
+            var id = new Guid();
+            var query = new GetPermissionByIdQuery(id);
+
+            mockAgg.setup_processor_to_verify_getUserQueries_are_the_same(id);
+
+            var controller = mockAgg.CreateUserController();
+
+            //Apply
+
+            var result = await controller.Get(id);
+
+            var projection = new UserDetailProjection()
+            {
+                FirstName = "Test",
+                LastName = "Name",
+                IsActive = true,
+                IsAdmin = true, 
+                Department = "Testeeee",
+                Position = "Deper",
+                Email ="asdasd@adad.com",
+                DateCreated = new DateTime(),
+                UserId = new Guid(),
+                PermissionList = "Permissions Here",
+            };
+
+            var newProjection = new UserDetailProjection()
+            {
+                FirstName = projection.FirstName,
+                LastName = projection.LastName,
+                IsActive = projection.IsActive,
+                IsAdmin = projection.IsAdmin,
+                Department = projection.Department,
+                Position = projection.Position,
+                Email = projection.Email,
+                DateCreated = projection.DateCreated,
+                UserId = projection.UserId,
+                PermissionList = projection.PermissionList
+            };
+
+            //await projection.ListenForEvents(new UserCreatedEvent
+            //{
+            //    FirstName = projection.FirstName,
+            //    LastName = projection.LastName,
+            //    IsActive = projection.IsActive,
+            //    IsAdmin = projection.IsAdmin,
+            //    Department = projection.Department,
+            //    Position = projection.Position,
+            //    Email = projection.Email,
+            //    Id = projection.UserId,
+            //    PermissionList = new Dictionary<Guid, PermissionDetails>()
+            //});
+
+            //await projection.ListenForEvents(new UserUpdatedEvent
+            //{
+            //    FirstName = projection.FirstName,
+            //    LastName = projection.LastName,
+            //    IsAdmin = projection.IsAdmin,
+            //    Department = projection.Department,
+            //    Position = projection.Position,
+            //    Email = projection.Email,
+            //    Id = projection.UserId,
+            //});
+            //await projection.ListenForEvents(new UserPermissionGrantedEvent());
+            //await projection.ListenForEvents(new UserPermissionRequestDeniedEvent());
+            //await projection.ListenForEvents(new UserPermissionRevokedEvent());
+            //await projection.ListenForEvents(new UserDisabledEvent());
+            //await projection.ListenForEvents(new UserEnabledEvent());
+
+            //Assert
+            //Assert.Equal(query, mockAgg.PermissionQuery);
+            Assert.IsType<UserDetailProjectionDTO>(result);
+            Assert.Equal(result.UserId, id);
+        }
+
+        [Fact]
+        public async void given_get_user_collection_query_queryprocessor_should_get_same_query_created_in_controller()
+        {
+            //Assemble
+            var mockAgg = new UserControllerMockAggregate();
+
+            mockAgg.setup_processor_to_verify_getUserCollectionQueries_are_the_same();
+
+            var controller = mockAgg.CreateUserController();
+
+            var filter = new UserFilterCriteria();
+
+            //Apply
+            var result = await controller.GetUserCollection(filter);
+
+            //Assert
+            Assert.IsType<List<UserDetailProjectionDTO>>(result);
+        }
+
+        [Fact]
+        public async void given_getoffboarduserpermissionsquery_queryprocessor_should_get_same_query_created_in_controller()
+        {
+            //Assemble
+            var mockAgg = new UserControllerMockAggregate();
+
+            var id = new Guid();
+            var query = new GetOffboardUserPermissionsQuery(id);
+
+            mockAgg.setup_processor_to_verify_getUserOffboardingQueries_are_the_same();
+
+            var controller = mockAgg.CreateUserController();
+
+            //Apply
+
+            var result = await controller.GetOffBoardingPermissionsById(id);
+
+            //Assert
+            //Assert.Equal(query, mockAgg.PermissionQuery);
+            Assert.IsType<OffBoardUserDTO>(result);
+        }
+    }
+}
