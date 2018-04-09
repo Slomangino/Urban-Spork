68b6682e3fbd385adb74173a28b121cd72ecf8d4
Stephen Lomangino <smlomangino1261@eagle.fgcu.edu>
2018-04-09 14:25:33 -0400

    Added getter for UserHistoryProjection along with query, queryhandler and filter, Added some regions in startup

---
+++ api/UrbanSpork.Common/FilterCriteria/UserHistoryFilterCriteria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanSpork.Common.FilterCriteria
+{
+    public class UserHistoryFilterCriteria
+    {
+        public Guid UserId { get; set; } = Guid.Empty;
+    }
+}
